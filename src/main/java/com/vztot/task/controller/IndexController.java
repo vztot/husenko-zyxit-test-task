7f50adf1613493e95a8be3f6c99252a7b7856fab
Oleksandr Husenko <oleksandrhusenko@gmail.com>
2020-08-07 15:52:27 +0300

    added views and some logic

--- src/main/java/com/vztot/task/controller/IndexController.java
+++ src/main/java/com/vztot/task/controller/IndexController.java
@@ -3,24 +3,31 @@ package com.vztot.task.controller;
 import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-    private String appMode;
+    final private String appMode;
 
     @Autowired
     public IndexController(Environment environment) {
         appMode = environment.getProperty("app-mode");
     }
 
-    @RequestMapping("/index")
-    public String getLoginPage(Model model) {
+    @GetMapping("/index")
+    public String getIndex() {
+        return "redirect:/";
+    }
+
+    @RequestMapping("/")
+    public String getLoginPage(Model model, Authentication authentication) {
         model.addAttribute("datetime", new Date());
-        model.addAttribute("username", "Ömerrrr");
-        model.addAttribute("mode", appMode);
+        model.addAttribute("username", authentication.getName());
+        //model.addAttribute("mode", appMode);
         return "index";
     }
 }
